a61f7419c2724a81e8815c2face31302f1cec73a
tabyouto <tabyouto@gmail.com>
2017-05-14 10:18:53 +0800

    init

---
+++ meta.js
@@ -0,0 +1,49 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A gl project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    },
+    "build": {
+      "type": "list",
+      "message": "Vue build",
+      "choices": [
+        {
+          "name": "Runtime + Compiler: recommended for most users fis3 server start && fis3 release -wL",
+          "value": "standalone",
+          "short": "standalone"
+        }
+      ]
+    }
+  },
+  "filters": {
+    ".eslintrc.js": "lint",
+    ".eslintignore": "lint",
+    "config/test.env.js": "unit || e2e",
+    "test/unit/**/*": "unit",
+    "build/webpack.test.conf.js": "unit",
+    "test/e2e/**/*": "e2e",
+    "src/router/**/*": "router"
+  },
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+};
